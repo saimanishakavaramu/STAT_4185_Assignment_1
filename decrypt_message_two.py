3e6c54e266b4244cfba8ca350aad14c8cd96e489
Saimanish Akavaramu <saimanish@MacBook-Air.local>
2023-01-30 00:03:51 -0500

    competed encrypted message two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,11 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+start = 1
+end = len(encrypted_message) - 2
+encrypted_message_list = list(encrypted_message)
+while (end - start >= 0):
+    encrypted_message_list[start], encrypted_message_list[end] = encrypted_message_list[end], encrypted_message_list[start]
+    start += 2
+    end -= 2
+print(''.join(encrypted_message_list))
